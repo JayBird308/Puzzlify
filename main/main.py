1bc331e59b3715a61c6f550de9bbbb7368ba2cf8
James Allan <s1032252@monmouth.edu>
2022-10-24 09:41:05 -0400

    Co-authored-by: DN002 <DN002@users.noreply.github.com> Co-authored-by: bswent18 <bswent18@users.noreply.github.com>

--- main/main.py
+++ main/main.py
@@ -1,2 +1,24 @@
 #live share test
-cdnjndjnd
+
+import pygame
+from sys import exit
+
+
+pygame.init()
+screen = pygame.display.set_mode((width,height))
+clock = pygame.time.Clock()
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+
+        #game code
+
+
+        pygame.display.update()
+        clock.tick(60)
+
