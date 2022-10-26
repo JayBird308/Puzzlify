691e37a696ddb2760efd4ab192a42e5ac1c77731
James Allan <s1032252@monmouth.edu>
2022-10-26 09:03:50 -0400

    committed

--- main/main.py
+++ main/main.py
@@ -7,15 +7,19 @@ from sys import exit
 width = 600;
 height = 400;
 
+def set_difficulty(value, difficulty):
+    pass
+
+def game_start():
+    pass
+
+menu = pygame_menu.Menu('Puzzlify', 400, 300, theme = pygame_menu.themes.THEME_GREEN)
+menu.add.button('Play', game_start)
+menu.add.button('Quit', pygame_menu.events.EXIT)
+
 pygame.init()
 screen = pygame.display.set_mode((width,height))
 clock = pygame.time.Clock()
-
-def set_difficulty(value, difficulty):
-
-pass
-
-
 
 while True:
     for event in pygame.event.get():
