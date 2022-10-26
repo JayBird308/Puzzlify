3fabd4d26e7109123c66ec37afb4d310645deea5
James Allan <s1032252@monmouth.edu>
2022-10-26 09:21:16 -0400

    menu example

--- main/main.py
+++ main/main.py
@@ -7,32 +7,23 @@ from sys import exit
 width = 600;
 height = 400;
 
+pygame.init()
+screen = pygame.display.set_mode((width,height))
+clock = pygame.time.Clock()
+
 def set_difficulty(value, difficulty):
     pass
 
 def game_start():
     pass
 
-menu = pygame_menu.Menu('Puzzlify', 400, 300, theme = pygame_menu.themes.THEME_GREEN)
+menu = pygame_menu.Menu('Puzzlify', 400, 300, theme = pygame_menu.themes.THEME_BLUE)
 menu.add.button('Play', game_start)
 menu.add.button('Quit', pygame_menu.events.EXIT)
 
-pygame.init()
-screen = pygame.display.set_mode((width,height))
-clock = pygame.time.Clock()
-
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            exit()
+menu.mainloop(screen)
 
 
-        #game code
-
-
-        pygame.display.update()
-        clock.tick(60)
 
 
 
