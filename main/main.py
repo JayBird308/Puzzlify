532e057245ab2a62e018db1dc176956563baa372
James Allan <s1032252@monmouth.edu>
2022-11-02 09:37:38 -0400

    11/2/22

--- main/main.py
+++ main/main.py
@@ -4,24 +4,43 @@ import pygame
 import pygame_menu
 from sys import exit
 
-width = 600;
-height = 400;
-
-pygame.init()
-screen = pygame.display.set_mode((width,height))
-clock = pygame.time.Clock()
-
 def set_difficulty(value, difficulty):
     pass
 
 def game_start():
     pass
 
-menu = pygame_menu.Menu('Puzzlify', 400, 300, theme = pygame_menu.themes.THEME_BLUE)
-menu.add.button('Play', game_start)
-menu.add.button('Quit', pygame_menu.events.EXIT)
+def main():
 
-menu.mainloop(screen)
+    #initialize the pygame module
+    pygame.init()
+
+    #load and set pygame logo
+    logo = pygame.image.load("logo32x32.png")
+    pygame.display.set_icon(logo)
+    pygame.display.set_caption("minimal program")
+
+    #height and width dimensions of application window
+    width = 600;
+    height = 400;
+
+    #create surface on screen of size height x width
+    screen = pygame.display.set_mode((width,height))
+
+    running = True
+
+    while running:
+
+
+    pygame.init()
+    clock = pygame.time.Clock()
+
+    menu = pygame_menu.Menu('Puzzlify', 400, 300, theme = pygame_menu.themes.THEME_BLUE)
+    menu.add.button('Play', game_start)
+    menu.add.button('Quit', pygame_menu.events.EXIT)
+    menu.mainloop(screen)
+
+
 
 
 
