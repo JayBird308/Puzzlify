f1ce1c54da22c77e6e3ae05c4bd15d174a962629
JayBird308 <s1032252@monmouth.edu>
2022-11-07 13:54:32 -0500

    menu theme

---
+++ main/theme.py
@@ -0,0 +1,8 @@
+import pygame
+import pygame_menu
+from sys import exit
+
+# mytheme = Theme(background_color = (0,0,0,0),
+                # title_background_color = (4,47,126),
+                # title_font_shadow=True,
+                # widget_padding=25)
