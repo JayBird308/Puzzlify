d2c730f83c3a38e42469d1805e10d763dcd4e90f
JayBird308 <s1032252@monmouth.edu>
2022-11-08 14:32:18 -0500

    menu stuff

--- main/customMenu_theme.py
+++ main/customMenu_theme.py
@@ -1,7 +1,7 @@
 import pygame_menu
 
 customImage = pygame_menu.baseimage.BaseImage(image_path= r'C:\Users\jtall\OneDrive\Documents\GitHub\Puzzlify\main\images\logo.png',
-                                          drawing_mode=pygame_menu.baseimage.IMAGE_MODE_FILL)
+                                          drawing_mode=pygame_menu.baseimage.IMAGE_MODE_CENTER)
 
 customMenu_theme = pygame_menu.Theme(background_color = (0,0,0,0), #transparent color
                             title_background_color = (255,0,0),
