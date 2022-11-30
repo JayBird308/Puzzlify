556fa050a4c46fc97ee98b9948d7c78bcc566d20
James Allan <s1032252@monmouth.edu>
2022-11-30 08:50:46 -0500

    fixed screen refreshing

--- main/main.py
+++ main/main.py
@@ -9,6 +9,7 @@ global USER32, WIDTH, HEIGHT
 USER32 = ctypes.windll.user32
 WIDTH = USER32.GetSystemMetrics(0) # Monitor Resolution Width
 HEIGHT = USER32.GetSystemMetrics(1) # Monitor Resolution Height
+SCREEN = pygame.display.set_mode((WIDTH,HEIGHT))
 
 # Math quiz
 def math_quiz():
@@ -17,53 +18,48 @@ def math_quiz():
 
 # game selection sub menu
 def game_select():
-    screen = pygame.display.set_mode((WIDTH,HEIGHT))
     gameMenu = pygame_menu.Menu('Game Selection', WIDTH, HEIGHT, theme = customMenu_theme)
     gameMenu.add.button('Math Quiz', math_quiz)
     gameMenu.add.button('Back', main)
-    gameMenu.mainloop(screen)
+    gameMenu.mainloop(SCREEN)
     pass
 
 # account sub menu
 def account_info():
-    screen = pygame.display.set_mode((WIDTH,HEIGHT))
     accountMenu = pygame_menu.Menu('Account', WIDTH, HEIGHT, theme = customMenu_theme)
     accountMenu.add.button('Account Login', account_login)
     accountMenu.add.button('Create an Account', account_create)
     accountMenu.add.button('Account Statistics', account_stats)
     accountMenu.add.button('Back', main)
-    accountMenu.mainloop(screen)
+    accountMenu.mainloop(SCREEN)
     pass
 
 # account creation sub menu
 def account_create():
-    screen = pygame.display.set_mode((WIDTH,HEIGHT))
     accountMenu = pygame_menu.Menu('Account Creation', WIDTH, HEIGHT, theme = customMenu_theme)
     accountMenu.add.text_input('Username: ', default = 'user')
     accountMenu.add.text_input('Password: ', default = 'password')
     accountMenu.add.text_input('Email Address: ', default = 'user@email.com')
     accountMenu.add.button('Submit Account', signup)
     accountMenu.add.button('Back', account_info)
-    accountMenu.mainloop(screen)
+    accountMenu.mainloop(SCREEN)
     pass
 
 # account login sub menu
 def account_login():
-    screen = pygame.display.set_mode((WIDTH,HEIGHT))
     accountMenu = pygame_menu.Menu('Account Login', WIDTH, HEIGHT, theme = customMenu_theme)
     accountMenu.add.text_input('Username: ', default = 'user')
     accountMenu.add.text_input('Password: ', default = 'password')
     accountMenu.add.button('Login', login)
     accountMenu.add.button('Back', account_info)
-    accountMenu.mainloop(screen)
+    accountMenu.mainloop(SCREEN)
     pass
 
 # account statistics sub menu
 def account_stats():
-    screen = pygame.display.set_mode((WIDTH, HEIGHT))
     accountMenu = pygame_menu.Menu('Account Statistics', WIDTH, HEIGHT, theme = customMenu_theme)
     accountMenu.add.button('Back', account_info)
-    accountMenu.mainloop(screen)
+    accountMenu.mainloop(SCREEN)
     pass
 
 # login button action for account database
@@ -87,9 +83,6 @@ def main():
     # load and set pygame window and clock
     clock = pygame.time.Clock()
 
-    # create surface on screen of size width x height
-    screen = pygame.display.set_mode((WIDTH,HEIGHT))
-
     # adds menu options
     menu = pygame_menu.Menu('Welcome ' + 'Account_Name' + '!', WIDTH, HEIGHT, theme = customMenu_theme)
     menu.add.button('Game Selection', game_select)
@@ -107,7 +100,7 @@ def main():
             if event.type == pygame.QUIT:
                     # set running condition to false, which will exit main loop
                     running = False
-        menu.mainloop(screen)
+        menu.mainloop(SCREEN)
         pygame.display.update()
         clock.tick(60)
 
