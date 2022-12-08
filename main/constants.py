f09533dcd730804fea9b6fd08a38df89087c6c3c
James T Allan <60233269+JayBird308@users.noreply.github.com>
2022-12-08 13:39:00 -0500

    reso

--- main/constants.py
+++ main/constants.py
@@ -6,12 +6,12 @@ pygame.init()
 
 USER32 = ctypes.windll.user32
 
-# WIDTH = USER32.GetSystemMetrics(0) # Monitor Resolution Width
-# HEIGHT = USER32.GetSystemMetrics(1) # Monitor Resolution Height
+WIDTH = USER32.GetSystemMetrics(0) # Monitor Resolution Width
+HEIGHT = USER32.GetSystemMetrics(1) # Monitor Resolution Height
 
 # Testing Window Dimensions
-WIDTH = 1280
-HEIGHT = 720
+# WIDTH = 1280
+# HEIGHT = 720
 
 SCREEN = pygame.display.set_mode((WIDTH,HEIGHT))
 FPS = 60
