130a1662ab308ae75c9fcfd48fdd5b8c36c362b0
James T Allan <60233269+JayBird308@users.noreply.github.com>
2023-01-20 13:15:53 -0500

    firebase

--- main/databaseConnection.py
+++ main/databaseConnection.py
@@ -1,5 +1,7 @@
-# import pyodbc
+from firebase_admin import db
 
+
+ref = db.reference("https://puzzlify-74c00-default-rtdb.firebaseio.com/")
 # Connection String
 # connection = pyodbc.connect('DRIVER={SQL Server}; SERVER=MSI\PUZZLIFYINSTANCE;DATABASE=Puzzlify; Trusted_Connection=yes;')
 
